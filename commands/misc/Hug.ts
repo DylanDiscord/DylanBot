786ac21fd7e2c6b29ea0b87c1086a58b07f795da
Stif <stifskere@gmail.com>
2023-01-16 22:21:13 +0100

    a

--- commands/misc/Hug.ts
+++ commands/misc/Hug.ts
@@ -1,5 +1,5 @@
 import {CommandBase, OCommandBuilder} from "../../handlers/CommandBase.js";
-import {EmbedBuilder, SlashCommandBuilder} from "discord.js";
+import {EmbedBuilder, SlashCommandBuilder, User} from "discord.js";
 import {Config, CustomMethods} from "../../exportMain.js";
 import {LanguageCodes} from "../../handlers/CustomMethods.js";
 
@@ -11,18 +11,22 @@ export default class Banana extends CommandBase {
 
     async run(): Promise<void> {
         
-        const user = this.context.options.getUser("usuario")
-        const embedE: EmbedBuilder = new EmbedBuilder()
-        .setDescription("No puedes abrazarte a ti mismo.")
-        .setColor(Config.colors.errorEmbed)
-        if(user!.id == this.context.user!.id) await this.context.reply({embeds: [embedE]})
+        const user: User = this.context.options.getUser("usuario")!;
+
+        if(user!.id == this.context.user!.id || user.bot) {
+            const embedE: EmbedBuilder = new EmbedBuilder()
+                .setDescription(`No puedes ${user.bot ? "abrazar a un bot" : "abrazarte a ti mismo"}`)
+                .setColor(Config.colors.errorEmbed);
+
+            await this.context.reply({embeds: [embedE]});
+            return;
+        }
 
         const embed: EmbedBuilder = new EmbedBuilder()
-        .setTitle(`${this.context.user?.tag} le dio un abrazo a ${user?.tag}`)
-        .setImage((await CustomMethods.getGifsFromTenor({query: "hug", locale: LanguageCodes.Spanish})).results[0].itemurl)
-        .setColor(Config.colors.successEmbed)
+            .setTitle(`${this.context.user?.tag} le dio un abrazo a ${user?.tag}`)
+            .setImage((await CustomMethods.getGifsFromTenor({query: "hug", locale: LanguageCodes.Spanish}))[0])
+            .setColor(Config.colors.successEmbed);
 
         await this.context.reply({embeds: [embed]});
-       
     }
 }
